e81afb1f6bf942cd5fffeff9979709fb0f026f50
Fene-87 <fenekayas87@gmail.com>
2023-06-13 13:43:13 +0300

    Create greetings component

--- src/components/greetings.js
+++ src/components/greetings.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Greetings = () => {
+  const greeting = useSelector((store) => store.greetings);
+
   return (
-    <div>greetings</div>
+    <div>
+        <h1>Here we go again</h1>
+        {greeting ? (<h2>greeting.message</h2>) : null}
+    </div>
   )
 }
 
